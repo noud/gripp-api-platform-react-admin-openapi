be3a3e189e0b089fda6fd2bc20472201522fa8f8
Noud de Brouwer <noud4@home.nl>
2020-04-22 19:50:41 +0000

    provision.sh

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
+rm -r gripp-admin
+rm -r node_modules package-lock.json yarn.lock
 npm install
 node_modules/.bin/create-react-app gripp-admin
-cd gripp-admin && yarn add @api-platform/admin@0.6.2
+
+cd gripp-admin
+rm -r node_modules package-lock.json yarn.lock
+yarn add @api-platform/admin@^0.6.2
+# yarn add @api-platform/admin@^2.1.0
+# yarn add eslint@^6.6.0 babel-eslint@*10.0.3
+
 cd .. && bin/edited.sh
-cd gripp-admin && yarn start
+cd gripp-admin && npm install && npm update && npm audit fix && yarn install && yarn start
