dd04ea3444464fe04fa896d0e47041ad1a540fa2
Noud de Brouwer <noud4@home.nl>
2020-04-23 23:21:36 +0200

    provision.sh

--- bin/provision.sh
+++ bin/provision.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 APP_NAME=gripp-admin
 
-rm -r ${APP_NAME}
-rm -r node_modules package-lock.json yarn.lock
+rm -r ${APP_NAME} node_modules package-lock.json yarn.lock
 
 npm install
 node_modules/.bin/create-react-app ${APP_NAME}
 
 cd ${APP_NAME}
-rm -r node_modules package-lock.json yarn.lock
-
 yarn add @api-platform/admin@^0.6.3
 # @todo update
 # yarn add @api-platform/admin@^1.0.0
